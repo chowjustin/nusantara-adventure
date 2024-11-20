57ed00085b92fe8f6ef5a659ebf6ae5b691f94f5
Justin Chow <justin.chow05@gmail.com>
2024-11-20 19:57:05 +0700

    initial commit

--- src/nusantara-adventure/nusantara-adventure/MainForm.Designer.cs
+++ src/nusantara-adventure/nusantara-adventure/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace nusantara_adventure
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "MainForm";
+            Text = "MainForm";
+            Load += MainForm_Load;
+            ResumeLayout(false);
         }
 
         #endregion
---
+++ src/nusantara-adventure/nusantara-adventure/MainForm.cs
@@ -0,0 +1,15 @@
+namespace nusantara_adventure
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
